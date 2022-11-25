4d33b9258b96522040429cbf1b273100a8d2e24c
Ravi Babu <baburavi27@gmail.com>
2022-11-25 18:55:59 +0530

    session 3 assignment

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const heading = React.createElement('h1', { id: 'title' }, 'Hello World');
+//const heading = React.createElement('h1', { id: 'title' }, 'Hello World');
+
+const TitleComponent = () => {
+    return (
+        <h1>Namaste React Bootcamp</h1>
+    );
+}
+
+const HeadingComponent = () => {
+    return (
+        <div id="title" className="title-class" tabIndex="1">
+            <TitleComponent />
+            <h2>from zero to hero</h2>
+            <h3>in months</h3>
+        </div>
+    );
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(heading);
+root.render(<HeadingComponent />);
